6dc61dbbbdee44a626f32207d30cea41d5a964af
Juho Nissi <juho_356@hotmail.com>
2014-09-24 10:17:23 +0300

    Versio 1

---
+++ Harj5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+void main()
+{
+	int A;
+	int B;
+	cout << "Syötä luvut A ja B";
+	cin >> A;
+	cin >> B;
+	if (A == B)
+		cout << "Bingo";
+	else cout << "Bongo";
+}
