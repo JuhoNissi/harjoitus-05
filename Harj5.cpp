e5d3256a3d500380d46c74e35c2ca1815a127d13
Juho Nissi <juho_356@hotmail.com>
2014-09-24 10:30:05 +0300

    Versio 2

--- Harj5.cpp
+++ Harj5.cpp
@@ -1,3 +1,13 @@
+/***********************************************
+* Ohjelman nimi: Harjoitus 5
+* Tekij‰: Juho Nissi
+*Lyhyt kuvaus:
+*Kirjoita ohjelma, joka pyyt‰‰ k‰ytt‰j‰lt‰ kaksi kokonaislukua.
+*Ohjelma tulostaa tekstin BINGO, mik‰li luvut ovat samoja.
+*Muussa tapauksessa ohjelma tulostaa tekstin BONGO.
+* Versio: 1.0
+* PVM: 17.9.2014
+************************************************/
 #include <iostream>
 using namespace std;
 void main()
